3bc106a746ad6244876387983060f7729f211fa6
Lora <lorageo33@gmail.com>
2025-09-23 19:22:05 +0300

    initial commit

--- src/main/java/ait/cohort51/g_51_security_jwt/security/filter/TokenFilter.java
+++ src/main/java/ait/cohort51/g_51_security_jwt/security/filter/TokenFilter.java
@@ -36,7 +36,9 @@ public class TokenFilter extends OncePerRequestFilter {
             UserDetails userDetails = userService.loadUserByUsername(username);
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails, null, userDetails.getAuthorities());
+                    userDetails, null, userDetails.getAuthorities()
+            );
+
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
